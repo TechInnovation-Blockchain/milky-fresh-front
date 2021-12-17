3f2fd1a502186f9b409ae291137eec954e572bfa
CeneOdie <ceneodendaal1@gmail.com>
2021-12-16 21:02:41 -0500

    color changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import {Container, Navbar, Nav, Button} from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div style = {{alignItems: "left"}}>
+        <div class = "milkynav" style = {{alignItems: "left"}}>
             <Navbar style = {{alignItems: "left"}}>
                 <Container>
                     <Navbar.Brand href="#home"><img src = {logo} alt="Milkyway" style={{ alignSelf: 'left' }}/></Navbar.Brand>
