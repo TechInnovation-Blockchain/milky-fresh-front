0d923ce58eeab90f80f5ccad83ad38490b598fe7
walkeraki <jurejure0@gmail.com>
2022-05-31 16:24:47 +0200

    Small fix

--- src/views/ComingSoon.tsx
+++ src/views/ComingSoon.tsx
@@ -1,9 +1,15 @@
 import ComingSoonObj from "sections/landing/ComingSoonObj";
+import { useEffect } from "react";
 
 //below home page style imported
 import style from "../views/coming_soon.module.css";
 
 const ComingSoon = () => {
+  useEffect(() => {
+    document.querySelector('html') ? document.querySelector('html')!.style.backgroundPositionY = '5%' : null;
+  }, []);
+
+
   return (<div className={style.content}>
     <ComingSoonObj />
   </div>)
